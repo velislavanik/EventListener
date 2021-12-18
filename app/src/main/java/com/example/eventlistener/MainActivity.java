64127a204fe67c3a608a6a8ee4f61f909310217f
Velis <velislava_n@abv.bg>
2021-12-18 10:52:23 +0200

    Listenre added in Main java

---
+++ app/src/main/java/com/example/eventlistener/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.eventlistener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.button);
+        Button b2 = findViewById(R.id.button2);
+
+        b.setOnClickListener(v -> {
+            TextView txtView = findViewById(R.id.textView);
+            txtView.setTextSize(20);
+        });
+
+        b2.setOnClickListener(v -> {
+            TextView txtView = findViewById(R.id.textView);
+            txtView.setTextSize(50);
+
+        });
+    }
+
+
+
+}
